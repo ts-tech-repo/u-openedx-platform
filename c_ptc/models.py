2ac1f7726cb3cb060ba8c1bfdcbd970b6b7e7688
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-07-21 17:12:40 +0530

    model & template changes

--- c_ptc/models.py
+++ c_ptc/models.py
@@ -12,12 +12,12 @@ class UserPtcInfo(models.Model):
         related_name="user_ptc_infos",
         db_column="userid",
     )
-    created_at = models.DateTimeField(default=timezone.now, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     ptc_type = models.CharField(max_length=80)
     submitted_at = models.DateTimeField(null=True, blank=True)
     course_ids = models.JSONField(default=list, blank=True)
     metadata = models.CharField(max_length=4000, default="{}")
-    modified_at = models.DateTimeField(default=timezone.now, auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.userid} - {self.ptc_type}"
