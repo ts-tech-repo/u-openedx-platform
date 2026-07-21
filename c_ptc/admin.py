611b009925a07cae13466d59d7edd1a2c84348cf
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-07-21 21:30:20 +0530

    admin panel changes

--- c_ptc/admin.py
+++ c_ptc/admin.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 User = get_user_model()
 
 
-class UserPtcInfoInline(admin.TabularInline):
+class UserPtcInfoInline(admin.StackedInline):
     model = UserPtcInfo
     extra = 0
     show_change_link = True
@@ -29,14 +29,19 @@ class UserPtcInfoInline(admin.TabularInline):
         "modified_at",
     )
 
+    verbose_name = "User PTC Info"
+    verbose_name_plural = "User PTC Info"
 
-# Append the inline to the existing User admin
+
+# Attach the inline to the existing User admin
 user_admin = admin.site._registry.get(User)
 log.warning("User admin = %s", user_admin)
+
 if user_admin:
-    if UserPtcInfoInline not in getattr(user_admin, "inlines", []):
-        user_admin.inlines = list(getattr(user_admin, "inlines", []))
-        user_admin.inlines.append(UserPtcInfoInline)
+    inlines = list(getattr(user_admin, "inlines", []))
+    if UserPtcInfoInline not in inlines:
+        inlines.append(UserPtcInfoInline)
+        user_admin.inlines = inlines
 
 
 @admin.register(UserPtcInfo)
@@ -46,4 +51,25 @@ class UserPtcInfoAdmin(admin.ModelAdmin):
         "ptc_type",
         "submitted_at",
         "created_at",
+    )
+
+    search_fields = (
+        "userid__username",
+        "userid__email",
+        "ptc_type",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "modified_at",
+    )
+
+    fields = (
+        "userid",
+        "ptc_type",
+        "submitted_at",
+        "course_ids",
+        "metadata",
+        "created_at",
+        "modified_at",
     )
