864f3491d971a60fb2abd8becfebcfaff6bb2d91
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-09-25 13:49:01 +0530

    fix

--- custom_lms/admin/learner_survey.py
+++ custom_lms/admin/learner_survey.py
@@ -1,9 +1,34 @@
 
 
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 from ..models.learner_survey import LearnerSurvey
+# --------------------------------------------------------------------------- #
+#  Common actions                                                              #
+# --------------------------------------------------------------------------- #
+
+def record_actions(obj):
+    update_url = reverse(
+        f"admin:{obj._meta.app_label}_{obj._meta.model_name}_change",
+        args=[obj.pk],
+    )
+    delete_url = reverse(
+        f"admin:{obj._meta.app_label}_{obj._meta.model_name}_delete",
+        args=[obj.pk],
+    )
+
+    return format_html(
+        '<a href="{}">Update</a>&nbsp;&nbsp;'
+        '<a href="{}" style="color:#ba2121;">Delete</a>',
+        update_url,
+        delete_url,
+    )
+
+
+record_actions.short_description = "Actions"
 
 @admin.register(LearnerSurvey)
 class LearnerSurveyAdmin(admin.ModelAdmin):
@@ -15,6 +40,7 @@ class LearnerSurveyAdmin(admin.ModelAdmin):
         "survey_id",
         "action",
         "created_at",
+        "record_actions",
     )
     list_filter = ("action", "survey_id", "created_at")
     search_fields = (
@@ -29,10 +55,12 @@ class LearnerSurveyAdmin(admin.ModelAdmin):
         "updated_at",
         "survey_uuid",
     )
-    date_hierarchy = "created_at"
-    ordering = ("-created_at",)
 
     def learner(self, obj):
         return f"{obj.user.username} ({obj.user.email})"
     learner.short_description = "Learner"
     learner.admin_order_field = "user__username"
+    
+    @admin.display(description="Actions")
+    def record_actions(self, obj):
+        return record_actions(obj)
